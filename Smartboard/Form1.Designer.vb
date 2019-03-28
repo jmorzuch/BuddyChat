b5e2efb34aff755b5add03d106ebd22c30eb4cf0
Joe Morzuch <jmorzuch@gmail.com>
2019-03-27 20:38:38 -0400

    Add project files.

---
+++ Smartboard/Form1.Designer.vb
@@ -0,0 +1,177 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Smartboard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Conversation_ListBox = New System.Windows.Forms.ListBox()
+        Me.MyPort = New System.Windows.Forms.TextBox()
+        Me.BuddyPort = New System.Windows.Forms.TextBox()
+        Me.BuddyIP = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Set_Button = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ISay_Box = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Control
+        Me.Panel1.Controls.Add(Me.Conversation_ListBox)
+        Me.Panel1.Controls.Add(Me.MyPort)
+        Me.Panel1.Controls.Add(Me.BuddyPort)
+        Me.Panel1.Controls.Add(Me.BuddyIP)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Set_Button)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.ISay_Box)
+        Me.Panel1.Location = New System.Drawing.Point(1, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(237, 561)
+        Me.Panel1.TabIndex = 0
+        '
+        'Conversation_ListBox
+        '
+        Me.Conversation_ListBox.Enabled = False
+        Me.Conversation_ListBox.FormattingEnabled = True
+        Me.Conversation_ListBox.Location = New System.Drawing.Point(7, 158)
+        Me.Conversation_ListBox.Name = "Conversation_ListBox"
+        Me.Conversation_ListBox.Size = New System.Drawing.Size(225, 329)
+        Me.Conversation_ListBox.TabIndex = 14
+        '
+        'MyPort
+        '
+        Me.MyPort.Location = New System.Drawing.Point(102, 69)
+        Me.MyPort.Name = "MyPort"
+        Me.MyPort.Size = New System.Drawing.Size(121, 20)
+        Me.MyPort.TabIndex = 13
+        '
+        'BuddyPort
+        '
+        Me.BuddyPort.Location = New System.Drawing.Point(64, 45)
+        Me.BuddyPort.Name = "BuddyPort"
+        Me.BuddyPort.Size = New System.Drawing.Size(159, 20)
+        Me.BuddyPort.TabIndex = 12
+        '
+        'BuddyIP
+        '
+        Me.BuddyIP.Location = New System.Drawing.Point(64, 20)
+        Me.BuddyIP.Name = "BuddyIP"
+        Me.BuddyIP.Size = New System.Drawing.Size(159, 20)
+        Me.BuddyIP.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(4, 141)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(69, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Conversation"
+        '
+        'Set_Button
+        '
+        Me.Set_Button.Location = New System.Drawing.Point(6, 95)
+        Me.Set_Button.Name = "Set_Button"
+        Me.Set_Button.Size = New System.Drawing.Size(226, 23)
+        Me.Set_Button.TabIndex = 9
+        Me.Set_Button.Text = "Set"
+        Me.Set_Button.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(4, 69)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(91, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "My Listening Port:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(3, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Buddy Port:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(4, 24)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Buddy IP:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(4, 499)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "I Say:"
+        '
+        'ISay_Box
+        '
+        Me.ISay_Box.Location = New System.Drawing.Point(7, 518)
+        Me.ISay_Box.Name = "ISay_Box"
+        Me.ISay_Box.Size = New System.Drawing.Size(216, 20)
+        Me.ISay_Box.TabIndex = 3
+        '
+        'Smartboard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Smartboard"
+        Me.Text = "Smartboard By Dr.  Lin"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ISay_Box As System.Windows.Forms.TextBox
+    Friend WithEvents MyPort As System.Windows.Forms.TextBox
+    Friend WithEvents BuddyPort As System.Windows.Forms.TextBox
+    Friend WithEvents BuddyIP As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Set_Button As System.Windows.Forms.Button
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Conversation_ListBox As System.Windows.Forms.ListBox
+
+End Class
---
+++ Smartboard/Form1.vb
@@ -0,0 +1,242 @@
+﻿Imports System
+Imports System.Net
+Imports System.Text
+Imports System.Net.Sockets
+Imports System.Threading
+Imports System.Collections.Generic
+
+Public Class Smartboard
+    Structure aline
+        Public x1 As Integer
+        Public y1 As Integer
+        Public x2 As Integer
+        Public y2 As Integer
+    End Structure
+    Dim new_line As Boolean
+    Dim connected As Boolean
+    Dim drawing As New List(Of aline)
+    Dim x1, y1, x2, y2 As Integer
+    Dim udpClient As New UdpClient
+    Dim GLOIP As IPAddress
+    Dim GLOINTPORT As Integer
+    Public receiveUdpClient As UdpClient
+    Public RemoteIPEndPoint As New IPEndPoint(IPAddress.Any, 0)
+    Public ThreadReceive As Thread
+    Public ReceivedCommand As String
+    Public ReceivedMessage As String
+    Public Received As Boolean
+    Public ListenPort As Integer
+    Public run_thread As Boolean = True
+    Public receiveClose As Boolean = False
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ClientSize = New Size(800, 600)
+        new_line = False
+        DoubleBuffered = True
+        Received = False
+        connected = False
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        x1 = e.X
+        y1 = e.Y
+        new_line = True
+    End Sub
+
+    Private Sub Form1_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        x2 = e.X
+        y2 = e.Y
+        Dim line As New aline
+        line.x1 = x1
+        line.x2 = x2
+        line.y1 = y1
+        line.y2 = y2
+        drawing.Add(line)
+        new_line = False
+        Send_Coordinate(x1, y1, x2, y2)
+        Invalidate()
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If new_line Then
+            x2 = e.X
+            y2 = e.Y
+            Dim line As New aline
+            line.x1 = x1
+            line.x2 = x2
+            line.y1 = y1
+            line.y2 = y2
+            drawing.Add(line)
+            Send_Coordinate(x1, y1, x2, y2)
+            x1 = x2
+            y1 = y2
+            Invalidate()
+        End If
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        Dim g As Graphics = e.Graphics
+        Dim draw As aline
+        If Received Then
+            Received = False
+
+            Select Case ReceivedCommand
+                Case = "Message"
+                    Conversation_ListBox.Items.Add(ReceivedMessage)
+                Case = "Close"
+                    Conversation_ListBox.Items.Add("Connected user is closing the application")
+                    Thread.Sleep(3000)
+                    Application.Exit()
+                Case = "Draw"
+                    draw.x1 = Integer.Parse(ReceivedMessage.Substring(ReceivedMessage.IndexOf("(") + 1, ReceivedMessage.IndexOf(",") - (ReceivedMessage.IndexOf("(") + 1)))
+                    draw.y1 = Integer.Parse(ReceivedMessage.Substring(ReceivedMessage.IndexOf(",") + 1, ReceivedMessage.IndexOf(")") - (ReceivedMessage.IndexOf(",") + 1)))
+                    draw.x2 = Integer.Parse(ReceivedMessage.Substring(ReceivedMessage.LastIndexOf("(") + 1, ReceivedMessage.LastIndexOf(",") - (ReceivedMessage.LastIndexOf("(") + 1)))
+                    draw.y2 = Integer.Parse(ReceivedMessage.Substring(ReceivedMessage.LastIndexOf(",") + 1, ReceivedMessage.LastIndexOf(")") - (ReceivedMessage.LastIndexOf(",") + 1)))
+                    drawing.Add(draw)
+            End Select
+        End If
+        If new_line Then
+            g.DrawLine(Pens.Blue, x1, y1, x2, y2)
+        End If
+        For Each line As aline In drawing
+            g.DrawLine(Pens.Black, line.x1, line.y1, line.x2, line.y2)
+        Next
+
+    End Sub
+
+    Private Sub Set_Button_Click(sender As Object, e As EventArgs) Handles Set_Button.Click
+        GLOIP = IPAddress.Parse(BuddyIP.Text)
+        GLOINTPORT = BuddyPort.Text
+
+        If (MyPort.Text <> "") Then
+            ListenPort = MyPort.Text
+
+            'Create a receive thread
+            ThreadReceive = New Thread(AddressOf ReceiveMessages)
+            Received = False
+            ThreadReceive.Start()
+            Set_Button.Enabled = False
+            connected = True
+        Else
+            Conversation_ListBox.Items.Add("Must set proper listening port!")
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub ReceiveMessages()
+        Dim receivingUdpClient As New UdpClient(ListenPort)
+
+        Do While run_thread
+            Try
+                Dim receiveBytes As Byte() = receivingUdpClient.Receive(RemoteIPEndPoint)
+
+                'Convert the received message to human readable format
+                Dim returnData As String = Encoding.ASCII.GetString(receiveBytes)
+
+                'Figure out what is being sent:
+                If (returnData.Substring(0, returnData.IndexOf(";")) = "Message") Then
+                    ReceivedCommand = "Message"
+                    returnData = returnData.Substring(8, returnData.Length - 8)
+                ElseIf (returnData.Substring(0, returnData.IndexOf(";")) = "Close") Then
+                    ReceivedCommand = "Close"
+                    returnData = returnData.Substring(6, returnData.Length - 6)
+                    receiveClose = True
+                ElseIf (returnData.Substring(0, returnData.IndexOf(";")) = "Draw") Then
+                    ReceivedCommand = "Draw"
+                    returnData = returnData.Substring(5, returnData.Length - 5)
+                End If
+
+                'RemoteIdEndPoint is your buddy's IP address
+                ReceivedMessage = RemoteIPEndPoint.Address.ToString() + " says: " + returnData
+
+                'A message is received, set flag to true
+                Received = True
+
+                'Ask program to update the output
+                Me.Invalidate()
+            Catch ex As Exception
+                ReceivedMessage = ex.ToString()
+                Me.Invalidate()
+            End Try
+        Loop
+    End Sub
+
+    Private Sub Send_Abort()
+        If connected Then
+
+            Dim bytCommand As Byte() = New Byte() {}
+
+            'Connect
+            udpClient.Connect(GLOIP, GLOINTPORT)
+
+            'Get message from I Say box, encode it
+            bytCommand = Encoding.ASCII.GetBytes("Close;Connected user initiated close, application will close in 3 seconds!")
+
+            'Send
+            udpClient.Send(bytCommand, bytCommand.Length)
+
+            If (receiveClose) Then
+                Thread.Sleep(3000)
+            Else
+                Thread.Sleep(6000)
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Send_Coordinate(x1 As Integer, y1 As Integer, x2 As Integer, y2 As Integer)
+        If connected Then
+
+            Dim bytCommand As Byte() = New Byte() {}
+
+            'Connect
+            udpClient.Connect(GLOIP, GLOINTPORT)
+
+            'Get message from I Say box, encode it
+
+            Dim tempLine As String
+            tempLine = "(" + x1.ToString + "," + y1.ToString + ")(" + x2.ToString + "," + y2.ToString + ")"
+            bytCommand = Encoding.ASCII.GetBytes("Draw;" + tempLine)
+            'Send
+            udpClient.Send(bytCommand, bytCommand.Length)
+
+
+        End If
+    End Sub
+
+    Private Sub Send_Text(Message As String)
+        If connected Then
+            Dim bytCommand As Byte() = New Byte() {}
+
+            'Connect
+            udpClient.Connect(GLOIP, GLOINTPORT)
+
+            'Get message from I Say box, encode it
+            bytCommand = Encoding.ASCII.GetBytes(Message)
+
+            'Send
+            udpClient.Send(bytCommand, bytCommand.Length)
+        End If
+
+    End Sub
+
+    Private Sub Smartboard_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If run_thread Then
+            Send_Abort()
+        End If
+    End Sub
+
+    Private Sub ISay_Box_KeyDown(sender As Object, e As KeyEventArgs) Handles ISay_Box.KeyDown
+        If e.KeyValue = 13 Then
+            e.SuppressKeyPress = True
+            Send_Text("Message;" + ISay_Box.Text)
+            ' Add your message to the conversation box
+            Conversation_ListBox.Items.Add("I Say:" + ISay_Box.Text)
+            ISay_Box.Text = ""
+            Invalidate()
+        End If
+    End Sub
+End Class
